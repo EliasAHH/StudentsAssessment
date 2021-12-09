28a7d259b1e02f0f3f464b464202faaba69a50f2
EliasAHH <eliasiscoding@gmail.com>
2021-12-08 19:55:52 -0800

    pictures are showing up and added some styles

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,10 +1,10 @@
-
+import  "./Student.css";
 
 const Student = (props) => {
 
    console.log(props.student);
 
-   const { city, company, email, id, lastname, pic, skill, grades} =  props.student;
+   const { city, company, email, id, firstName,  lastName, pic, skill, grades} =  props.student;
 
    const getAverage = () =>  {
         return grades.reduce((acc, curr) => {
@@ -12,12 +12,30 @@ const Student = (props) => {
        },0) / grades.length
    }
 
+   const displayGrades = () => {
+    return grades.map((g,index) =>{
+        return(<li>
+                Test {index+1}: {g}%
+            </li>
+        )
+    })}
+
+
      return(
-         <div>
-             <img src={pic} alt="Icon for students" /> City: {city} <br/>
-             Company: {company} <br/>
-             Email: {email }<br/> <br/>
-             Average: {getAverage()}%
+         <div className="student-container">
+             <div className="student-image">
+                 <img src={pic} alt="Icon for students" />
+            </div> 
+             <div className="student-info">
+                <h1>{firstName} {lastName}</h1>
+                Email: {email }<br/>
+                Company: {company} <br/>
+                Skill: {skill} <br/>
+                Average: {getAverage()}%<br/>
+                <ul>
+                {displayGrades()}
+                </ul>
+            </div>
          </div>
      )
 }
